aeca7f535da3917291309a7132bc239b7a52d999
kennykarnama <kenny@ruangguru.com>
2019-06-09 18:03:35 +0700

    Plotting on python consumer

--- receive.py
+++ receive.py
@@ -1,18 +1,37 @@
 import pika
+import matplotlib.pyplot as plt
+
 credentials = pika.PlainCredentials('guest', 'guest')
 connection = pika.BlockingConnection(
-            pika.ConnectionParameters('localhost',5672,'/',credentials))
+    pika.ConnectionParameters('localhost',5672,'/',credentials))
+
 channel = connection.channel()
 
 channel.queue_declare(queue='plot-me')
 
+images = 1
+
+def restoreData(delimitedString):
+    signals = delimitedString.split(",")
+    signals = [float(x) for x in signals]
+    return signals
+
+def plotSignal(signal):
+    global images
+    plt.figure(1)
+    plt.title('Signal Wave...')
+    plt.plot(signal)
+    plt.savefig("signal_"+str(images)+".png")
+    images = images+1
 
 def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+    signals = restoreData(str(body, 'utf-8'))
+    print("Plotting signals...")
+    plotSignal(signals)
 
 
-        channel.basic_consume(
-                    queue='hello', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(
+    queue='plot-me', on_message_callback=callback, auto_ack=True)
 
-        print(' [*] Waiting for messages. To exit press CTRL+C')
-        channel.start_consuming()
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming() 
